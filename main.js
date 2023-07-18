fb6efc37ecd4d83faf357c7728b943fd3fbe8cb8
NowWeAre-Free <kontodarm@wp.pl>
2023-07-18 22:39:28 +0200

    add setTimeout and if statement

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 // http://websamuraj.pl/examples/js/projekt8/
-// Uzyskaj efekt jak pod tym linkiem 
+// Uzyskaj efekt jak pod tym linkiem
 // Użyj setTimeout zamiast setInterval,
 // Opóźniej start pisanie każdego słowa
 // Uwaga: Zadanie bardzo trudne
 
-const spnText = document.querySelector('.text');
-const spnCursor = document.querySelector('.cursor');
-const txt = ['tekst1', 'tekst2', 'tekst3']
+const spnText = document.querySelector(".text");
+const spnCursor = document.querySelector(".cursor");
+const txt = ["tekst1", "tekst2", "tekst3"];
+
+let activeText = 0;
+let activeLetter = 0;
 
 // Implementacja
 const addLetter = () => {
- // Użyj w środku setTimeout
-}
-
+  // Użyj w środku setTimeout
+  spnText.textContent += txt[activeText][activeLetter];
+  activeLetter++;
+  setTimeout(addLetter, activeLetter);
+  if (activeLetter === txt[activeText].length) {
+    activeLetter = 0;
+    activeText++;
+  }
+};
 
 addLetter(); //pierwsze wywołanie
 
-
 // Animacja kursora (zostaw bez zmian)
 const cursorAnimation = () => {
- spnCursor.classList.toggle('active');
-}
+  spnCursor.classList.toggle("active");
+};
 setInterval(cursorAnimation, 400);
